1b6ee2b8b4fa8bf27285b39cea050d7367f642b7
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2024-11-20 01:59:32 +0400

    add player turn printer

--- t3.asm
+++ t3.asm
@@ -1,15 +1,57 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Copyright (c) 2024, Farhad Mehdizada (@ferhadme) ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; A lightweight, terminal-based implementation of the classic Tic Tac Toe game written in Netwide Assembler
+
 section .data
-  hello_msg: db "Hello, World!", 10
+  board_len: equ 9
+  p_turn_msg_len: equ 15
+  row: db '-'
+  col: db '|'
+  p_turn_msg_prefix: db 'Player '
+  p_turn_msg_prefix_len: equ 7
+  p_turn_msg_suffix: db ' turn:', 10
+  p_turn_msg_suffix_len: equ 7
+  px: db 'X'
+  py: db 'Y'
+
+section .bss
+  p_turn resb 1
+  board resb board_len
+  p_turn_msg resb p_turn_msg_len
 
 section .text
 global _start
 _start:
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, hello_msg
-  mov rdx, 14
+
+  mov al, [px]
+
+change_turn:
+  mov [p_turn], al
+
+print_turn:
+
+  mov rsi, p_turn_msg_prefix
+  mov rdi, p_turn_msg
+  mov rcx, p_turn_msg_prefix_len
+  rep movsb
+
+  mov al, [p_turn]
+  mov [p_turn_msg + p_turn_msg_prefix_len], al
+
+  mov rsi, p_turn_msg_suffix
+  mov rdi, p_turn_msg + p_turn_msg_prefix_len + 1
+  mov rcx, p_turn_msg_suffix_len
+  rep movsb
+
+  mov rax, 0x01
+  mov rdi, 0x00
+  mov rsi, p_turn_msg
+  mov rdx, p_turn_msg_len
   syscall
 
-  mov rax, 60
+exit:
+  mov rax, 0x3c
   mov rdi, 0
   syscall
