09dbb0039cc27facc16fcbee457aaeb3649929d9
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2024-11-29 02:09:14 +0400

    fix print_board_templ

--- t3.asm
+++ t3.asm
@@ -7,28 +7,31 @@
 section .data
   board_len: equ 9
   p_turn_msg_len: equ 15
+
   row: db '-'
   col: db '|'
+
   p_turn_msg_prefix: db 'Player '
   p_turn_msg_prefix_len: equ 7
   p_turn_msg_suffix: db ' turn:', 10
   p_turn_msg_suffix_len: equ 7
   px: db 'X'
   py: db 'Y'
-  board_templ_row_len: equ 7
-  board_templ_col_len: equ 13
-  board_templ_len: equ 13 * 7
+
+  board_templ_row_len: equ 6
+  board_templ_col_len: equ 7 ; 7 + 1(\n)
+  board_templ_len: equ 8 * 6
 
 section .bss
   p_turn resb 1
 
-  ; -------------
-  ; | X | X | X |
-  ; -------------
-  ; | X | X | X |
-  ; -------------
-  ; | X | X | X |
-  ; -------------
+  ; -------\n
+  ; |X|X|X|\n
+  ; -------\n
+  ; |X|X|X|\n
+  ; -------\n
+  ; |X|X|X|\n
+  ; -------\n
   board_templ resb board_templ_len
   board resb board_len
   p_turn_msg resb p_turn_msg_len
@@ -61,25 +64,25 @@ print_turn:
   mov rdx, p_turn_msg_len
   syscall
 
-print_board_templ:
+print_board_templ: ;; 0 .. 3
+  mov rdi, board_templ
   mov rcx, 0
-print_row:
-  inc rcx
-  cmp rcx, board_templ_row_len
-  jg print_templ
+print_row: ;; 0 .. 6
   mov r8, 0
 print_bars:
-  mov rsi, board_templ ;; FIXME: Update rsi correctly
   mov al, [row]
-  mov [rsi + r8], al
+  mov [rdi + r8], al
   inc r8
   cmp r8, board_templ_col_len
   jl print_bars
   mov al, 10
-  mov [rsi + r8], al
-print_data:
-  jmp print_row
+  mov [rdi + r8], al
 print_templ:
+  add rdi, board_templ_col_len
+  inc rdi ; +1(\n)
+  inc rcx
+  cmp rcx, 3
+  jl print_row
   mov rax, 0x01
   mov rdi, 0x00
   mov rsi, board_templ
