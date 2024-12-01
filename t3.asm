2fb990fe7293ad7bc4356a853132de29ead1f42e
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2024-12-01 15:19:33 +0400

    implement stdout board template

--- t3.asm
+++ t3.asm
@@ -6,33 +6,36 @@
 
 section .data
   board_len: equ 9
+
+  bar: db '-'
+  sep: db '|'
+
+
   p_turn_msg_len: equ 15
-
-  row: db '-'
-  col: db '|'
-
   p_turn_msg_prefix: db 'Player '
   p_turn_msg_prefix_len: equ 7
   p_turn_msg_suffix: db ' turn:', 10
   p_turn_msg_suffix_len: equ 7
+
   px: db 'X'
   py: db 'Y'
 
-  board_templ_row_len: equ 6
-  board_templ_col_len: equ 7 ; 7 + 1(\n)
-  board_templ_len: equ 8 * 6
+  board_out_len: equ 56
+  out_row_len: equ 6 ; +1(tail)
+  out_col_len: equ 7 ; +1(\n)
 
 section .bss
   p_turn resb 1
 
-  ; -------\n
-  ; |X|X|X|\n
-  ; -------\n
-  ; |X|X|X|\n
-  ; -------\n
-  ; |X|X|X|\n
-  ; -------\n
-  board_templ resb board_templ_len
+  ; 01234567
+  ; -------\n 0
+  ; |X|X|X|\n 1
+  ; -------\n 2
+  ; |X|X|X|\n 3
+  ; -------\n 4
+  ; |X|X|X|\n 5
+  ; -------\n 6
+  board_templ resb board_out_len
   board resb board_len
   p_turn_msg resb p_turn_msg_len
 
@@ -40,6 +43,16 @@ section .text
 global _start
 _start:
   mov al, [px]
+
+  mov rcx, 0
+  mov rdi, board
+init_game:
+  mov al, 32
+  mov [rdi], al
+  inc rdi
+  inc rcx
+  cmp rcx, board_len
+  jl init_game
 
 change_turn:
   mov [p_turn], al
@@ -64,30 +77,77 @@ print_turn:
   mov rdx, p_turn_msg_len
   syscall
 
-print_board_templ: ;; 0 .. 3
+
+; 01234567
+; -------\n 0
+; |X|X|X|\n 1
+; -------\n 2
+; |X|X|X|\n 3
+; -------\n 4
+; |X|X|X|\n 5
+; -------\n 6
+
+print_game: ; 0..3
+  mov rsi, board
   mov rdi, board_templ
   mov rcx, 0
-print_row: ;; 0 .. 6
+print_row:
   mov r8, 0
 print_bars:
-  mov al, [row]
-  mov [rdi + r8], al
+  mov al, [bar]
+  mov [rdi], al
+  inc rdi
   inc r8
-  cmp r8, board_templ_col_len
+  cmp r8, out_col_len
   jl print_bars
   mov al, 10
-  mov [rdi + r8], al
-print_templ:
-  add rdi, board_templ_col_len
-  inc rdi ; +1(\n)
+  mov [rdi], al
+  inc rdi
+print_data:
+  mov r9, 0
+print_sep_and_data:
+  mov al, [sep]
+  mov [rdi], al
+  inc rdi
+  mov al, [board + r9]
+  mov [rdi], al
+  inc rdi
+  inc r9
+  cmp r9, 3
+  je print_sep_and_data_end
+  jmp print_sep_and_data
+print_sep_and_data_end:
+  mov al, [sep]
+  mov [rdi], al
+  inc rdi
+  mov al, 10
+  mov [rdi], al
+  inc rdi
+
   inc rcx
   cmp rcx, 3
   jl print_row
+
+  mov r8, 0
+print_tail_bar:
+  mov al, [bar]
+  mov [rdi], al
+  inc rdi
+  inc r8
+  cmp r8, out_col_len
+  jl print_tail_bar
+  mov al, 10
+  mov [rdi], al
+  inc rdi
+stdout:
   mov rax, 0x01
   mov rdi, 0x00
   mov rsi, board_templ
-  mov rdx, board_templ_len
+  mov rdx, board_out_len
   syscall
+
+game_stdin:
+
 
 check_winner:
 
